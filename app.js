02542cbfef75f57a857b84338b6606ab7fb4969d
iknowtavonga <67345561+iknowtavonga@users.noreply.github.com>
2020-09-22 21:52:36 +0200

    initiated javascript

---
+++ app.js
@@ -0,0 +1,6 @@
+// First we assign all our html content to variables
+
+const hero = document.querySelector('.hero');
+const slider = document.querySelector('.slider');
+const logo = document.querySelector('#logo');
+const hamburger = document.querySelector('.hamburger');
